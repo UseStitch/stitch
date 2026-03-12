8b64430c55e0a1a2012085597a3ab1d661d4af7e
Maharshi Patel <maharshi365@gmail.com>
2026-03-12 11:41:31 -0400

    added hono server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,10 @@
-export {};
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/health', (c) => c.json({ status: 'ok' }));
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+};
