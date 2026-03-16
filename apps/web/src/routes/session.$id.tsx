086d8f716a64bd38bfa48d6c8c6a7546ac4f8ede
Maharshi Patel <maharshi365@gmail.com>
2026-03-16 13:27:23 -0400

    style: enhance view transitions for session to session movement

--- apps/web/src/routes/session.$id.tsx
+++ apps/web/src/routes/session.$id.tsx
@@ -124,7 +124,7 @@ function SessionComponent() {
       initial="smooth"
     >
       <StickToBottom.Content scrollClassName="no-scrollbar" className="px-6 pb-40 pt-6">
-        <div className="mx-auto max-w-4xl">
+        <div className="mx-auto max-w-4xl" style={{ viewTransitionName: 'chat-thread' }}>
           <MessageList
             messages={messages}
             streamState={streamState}
