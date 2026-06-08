7e9c0aea76f9647389b7fa6acd1e7c2ea1ccd580
Maharshi Patel <maharshi365@gmail.com>
2026-06-08 14:06:04 -0400

    chore: update recordings settings route to prefetch sttProviderModels

--- apps/web/src/routes/settings/recordings.tsx
+++ apps/web/src/routes/settings/recordings.tsx
@@ -1,7 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 import { RecordingsSettings } from '@/components/settings/recordings';
-import { audioProviderModelsQueryOptions, transcriptionProviderModelsQueryOptions } from '@/lib/queries/providers';
+import {
+  audioProviderModelsQueryOptions,
+  sttProviderModelsQueryOptions,
+} from '@/lib/queries/providers';
 import { audioDevicesQueryOptions, audioPermissionsQueryOptions } from '@/lib/queries/recordings';
 import { settingsQueryOptions } from '@/lib/queries/settings';
 
@@ -9,7 +12,7 @@ export const Route = createFileRoute('/settings/recordings')({
   loader: ({ context }) =>
     Promise.all([
       context.queryClient.ensureQueryData(settingsQueryOptions),
-      context.queryClient.ensureQueryData(transcriptionProviderModelsQueryOptions),
+      context.queryClient.ensureQueryData(sttProviderModelsQueryOptions),
       context.queryClient.ensureQueryData(audioProviderModelsQueryOptions),
       context.queryClient.ensureQueryData(audioDevicesQueryOptions),
       context.queryClient.ensureQueryData(audioPermissionsQueryOptions),
