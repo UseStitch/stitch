ec7e6c62e677a4ba452102503a13a45eaa539e32
Maharshi Patel <maharshi365@gmail.com>
2026-07-06 20:06:38 -0400

    chore(db): consolidate archive migrations

--- packages/server/drizzle/0036_perfect_mauler.sql
+++ packages/server/drizzle/0036_perfect_mauler.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `messages` ADD `archived_at` integer;--> statement-breakpoint
 ALTER TABLE `messages` ADD `archived_reason` text;--> statement-breakpoint
-ALTER TABLE `sessions` ADD `archived_at` integer;
+ALTER TABLE `sessions` ADD `archived_at` integer;--> statement-breakpoint
+ALTER TABLE `sessions` ADD `archived_reason` text;
--- packages/server/drizzle/0037_motionless_piledriver.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `sessions` ADD `archived_reason` text;
